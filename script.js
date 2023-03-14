677ee5a8db25ab5327a3c00f5705e8872760a1e5
crescentarc <matthew.seth.taganile@gmail.com>
2023-03-14 12:22:38 -0400

    Add function that checks for valid phone number

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 // set error messages for incorrect password entry
 let password = document.querySelector(`#password`);
 let passwordConfirm = document.querySelector(`#password-confirm`);
-
 let passwordError = document.querySelector(`.password-error`);
 
 passwordConfirm.addEventListener(`input`, function() {
@@ -9,5 +8,17 @@ passwordConfirm.addEventListener(`input`, function() {
         passwordError.textContent = `Passwords do not match`;
     } else if (passwordConfirm.value == password.value) {
         passwordError.textContent = ``;
+    }
+})
+
+//set error message for incorrect phone number entry
+let phone = document.querySelector(`#phone`);
+let phoneError = document.querySelector(`.phone-error`);
+
+phone.addEventListener(`input`, function() {
+    if(phone.value.length < 10 || phone.value.length > 10) {
+        phoneError.textContent = `Phone number must be 10 numbers (No dashes)`;
+    } else if (phone.value.length == 10) {
+        phoneError.textContent = ``;
     }
 })
